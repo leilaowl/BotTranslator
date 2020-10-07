19d1bd5c0e6cb5ff1e00f45db6ea29c7433863a0
Leila Isaieva <lei24isa@gmail.com>
2020-10-07 19:34:16 +0300

    adding a possibility to select language

--- BotTranslator/bot_translate.py
+++ BotTranslator/bot_translate.py
@@ -1,12 +1,54 @@
 # this file is to demonstrate how translation works
-
 # Googletrans is a free and unlimited python library that implemented Google Translate API
 from googletrans import Translator
 
 translator = Translator()
 # print(translator.translate(message, dest='ru'))
 
+"""
+{'af': 'afrikaans', 'sq': 'albanian', 'am': 'amharic', 'ar': 'arabic', 'hy': 'armenian', 'az': 'azerbaijani',
+'eu': 'basque', 'be': 'belarusian', 'bn': 'bengali', 'bs': 'bosnian', 'bg': 'bulgarian', 'ca': 'catalan',
+'ceb': 'cebuano', 'ny': 'chichewa', 'zh-cn': 'chinese (simplified)', 'zh-tw': 'chinese (traditional)',
+'co': 'corsican', 'hr': 'croatian', 'cs': 'czech', 'da': 'danish', 'nl': 'dutch', 'en': 'english',
+'eo': 'esperanto', 'et': 'estonian', 'tl': 'filipino', 'fi': 'finnish', 'fr': 'french', 'fy': 'frisian',
+'gl': 'galician', 'ka': 'georgian', 'de': 'german', 'el': 'greek', 'gu': 'gujarati', 'ht': 'haitian creole',
+'ha': 'hausa', 'haw': 'hawaiian', 'iw': 'hebrew', 'hi': 'hindi', 'hmn': 'hmong', 'hu': 'hungarian',
+'is': 'icelandic', 'ig': 'igbo', 'id': 'indonesian', 'ga': 'irish', 'it': 'italian', 'ja': 'japanese', 
+jw': 'javanese', 'kn': 'kannada', 'kk': 'kazakh', 'km': 'khmer', 'ko': 'korean', 'ku': 'kurdish (kurmanji)',
+'ky': 'kyrgyz', 'lo': 'lao', 'la': 'latin', 'lv': 'latvian', 'lt': 'lithuanian', 'lb': 'luxembourgish',
+'mk': 'macedonian', 'mg': 'malagasy', 'ms': 'malay', 'ml': 'malayalam', 'mt': 'maltese', 'mi': 'maori',
+'mr': 'marathi', 'mn': 'mongolian', 'my': 'myanmar (burmese)', 'ne': 'nepali', 'no': 'norwegian', 'ps': 'pashto',
+'fa': 'persian', 'pl': 'polish', 'pt': 'portuguese', 'pa': 'punjabi', 'ro': 'romanian', 'ru': 'russian', 'sm': 'samoan',
+'gd': 'scots gaelic', 'sr': 'serbian', 'st': 'sesotho', 'sn': 'shona', 'sd': 'sindhi', 'si': 'sinhala', 'sk': 'slovak',
+'sl': 'slovenian', 'so': 'somali', 'es': 'spanish', 'su': 'sundanese', 'sw': 'swahili', 'sv': 'swedish', 'tg': 'tajik',
+'ta': 'tamil', 'te': 'telugu', 'th': 'thai', 'tr': 'turkish', 'uk': 'ukrainian', 'ur': 'urdu', 'uz': 'uzbek',
+'vi': 'vietnamese', 'cy': 'welsh', 'xh': 'xhosa', 'yi': 'yiddish', 'yo': 'yoruba', 'zu': 'zulu', 'fil': 'Filipino',
+'he': 'Hebrew'}
+"""
+
+"""
+@bot.message_handler(commands=['change_language'])  # /change_language command
+def change_lg(message):
+    bot.send_message(message.chat.id, 'С какого языка вы хотите перевести?')
+
+
+@bot.message_handler(content_types=['text'])  # translates text of the message to russian
+def f_lg(message):
+    selected_lan = message
+
+
+@bot.message_handler(commands=['change_language'])  # /change_language command
+def change_dest_lg(message):
+    bot.send_message(message.chat.id, 'На какой язык вы хотите перевести?')
+
+"""
+
+
+src_lg = input('С какого языка вы хотите перевести?:')
+dest_lg = input('На какой язык сделать перевод?')
+
+
 while True:
     message = input('')
-    result = translator.translate(message, dest='ru')
+    result = translator.translate(message, src=src_lg, dest=dest_lg)
     print(result.text)
--- BotTranslator/main.py
+++ BotTranslator/main.py
@@ -1,6 +1,7 @@
 # t.me/mow_tr_bot  - link to the bot
 from BotTranslator import config  # imports from config.py
 import telebot  # pyTelegramBotAPI
+from telebot import types
 from googletrans import Translator  # free and unlimited python library that implemented Google Translate API
 
 
@@ -10,11 +11,39 @@ translator = Translator()  # creates a new Translator object
 
 @bot.message_handler(commands=['start'])  # /start command
 def send_welcome(message):
-    bot.reply_to(message, 'Привет! Я помогу тебе с переводом. \nПока я перевожу только с английского на русский :)')
+    bot.reply_to(message, '''
+    Привет! Я помогу тебе с переводом. \nПока я перевожу только с английского на русский\
+    /change_language - комманда, чтобы выбрать\\сменить язык\
+    Успехов!''')
+
+
+keyboard = types.InlineKeyboardMarkup()
+keyboard.add(types.InlineKeyboardButton('Русский', callback_data='ru'),
+             types.InlineKeyboardButton('Английский', callback_data='en'),
+             types.InlineKeyboardButton('Немецкий', callback_data='de'),
+             types.InlineKeyboardButton('Украинский', callback_data='uk'))
+
+
+@bot.message_handler(commands=['change_language'])  # /change_language command
+def src_language(message):
+    cid = message.chat.id
+    bot.send_message(cid, 'С какого языка вы хотите перевести?', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call: True)
+def dest_language(call):
+    if call.data in ['ru', 'en', 'de', 'uk']:
+        bot.send_message(call.message.chat.id, text='На какой язык вы хотите перевести?', reply_markup=keyboard)
+
+
+@bot.callback_query_handler(func=lambda call:True)
+def call_answer(call):
+    if call.data in ['ru', 'en', 'de', 'uk']:
+        bot.send_message(call.message.chat.id, text="Теперь введите ваше сообщение")
 
 
 @bot.message_handler(content_types=['text'])  # translates text of the message to russian
-def translate(message):
+def bot_trans(message):
     result = translator.translate(message.text, dest='ru')
     trans_text = result.text
     bot.send_message(message.chat.id, trans_text)
